1c3a0dc37fbbc15b20e86029869fa493bd051965
Stanislav Melekhin <darkprado@yandex.ru>
2021-09-30 01:06:24 +0300

    init

---
+++ src/test/java/com/example/kafka_sender/KafkaSenderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.kafka_sender;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KafkaSenderApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
